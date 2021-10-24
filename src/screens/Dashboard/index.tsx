7fa4c9ba8631172076f16a3585b2baa6b8fb7580
cga <carolinagalmeida.eng@gmail.com>
2021-10-24 16:12:40 -0300

    initial-setup

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+import { Container } from "./styles";
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Text>Dashboard</Text>
+        </Container>
+    )
+}
